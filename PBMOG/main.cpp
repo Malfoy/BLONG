d3cc1a5fb0e86eb751483dc17c17bbf9855431f4
malfoy <alimasse@nadine-wifi.irisa.fr>
2014-12-16 16:02:57 +0100

    Initial Commit

---
+++ PBMOG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PBMOG
+//
+//  Created by malfoy on 16/12/2014.
+//  Copyright (c) 2014 malfoy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
